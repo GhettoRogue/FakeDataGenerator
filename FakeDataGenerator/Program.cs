9e74780afab4acd13196f01ac93f7e33c261b4bc
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-11-18 14:33:41 +0400

    upd

---
+++ FakeDataGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿using FakeDataGenerator;
+using FakeDataGenerator.creators;
+using FakeDataGenerator.generator;
+
+var factory = new PersonFactory(10);
+var repository = new PersonRepository(factory);
+
+var listPersons1 = repository.GetPersons().Take(2); 
+var listPersons2 = repository.GetPersonsWithAge().Take(2); 
+var listPersons3 = repository.GetPersonsFullInfo().Take(2); 
+
+Console.WriteLine();
+
+foreach (var person in listPersons1)
+{
+    Console.WriteLine($"{person.FirstName} {person.LastName} {person.Patronymic}"); 
+}
+
+Console.WriteLine();
+
+foreach (var person in listPersons2)
+{
+    Console.WriteLine($"{person.FirstName} {person.LastName} {person.Patronymic} : {person.Age}"); 
+}
+
+Console.WriteLine();
+
+foreach (var person in listPersons3)
+{
+    Console.WriteLine($"{person.FirstName} {person.LastName} {person.Patronymic} : {person.Age} - {person.PhoneNumber}"); 
+}
