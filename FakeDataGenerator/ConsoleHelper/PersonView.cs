ab0b88c542b29eeff069560fa34dd7eaef3c4ea8
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-11-21 16:41:05 +0400

    add PersonView.cs

---
+++ FakeDataGenerator/ConsoleHelper/PersonView.cs
@@ -0,0 +1,31 @@
+﻿using FakeDataGenerator.models;
+
+namespace FakeDataGenerator.ConsoleHelper;
+
+public static class PersonView
+{
+    
+    public static void PrintPerson(IEnumerable<Person> persons)
+    {
+        foreach (var person in persons)
+        {
+            Console.WriteLine($"{person.FirstName} {person.LastName} {person.Patronymic} {person.Age} {person.PhoneNumber}"); 
+        }
+    }
+    
+    public static void PrintPersonWithAge(IEnumerable<Person> persons)
+    {
+        foreach (var person in persons)
+        {
+            Console.WriteLine($"{person.FirstName} {person.LastName} {person.Patronymic} {person.Age}"); 
+        }
+    }
+    
+    public static void PrintPersonWithAgeAndName(IEnumerable<Person> persons)
+    {
+        foreach (var person in persons)
+        {
+            Console.WriteLine($"{person.FirstName} {person.Age}"); 
+        }
+    }
+}
