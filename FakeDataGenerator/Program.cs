a6b2d7f1fafc33f3076538554757528a899922b4
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-11-21 16:41:30 +0400

    upd testing

--- FakeDataGenerator/Program.cs
+++ FakeDataGenerator/Program.cs
@@ -1,31 +1,22 @@
-﻿using FakeDataGenerator;
+﻿using FakeDataGenerator.ConsoleHelper;
 using FakeDataGenerator.creators;
 using FakeDataGenerator.generator;
 
-var factory = new PersonFactory(10);
+var factory = new PersonFactory(5);
 var repository = new PersonRepository(factory);
 
-var listPersons1 = repository.GetPersons().Take(2); 
-var listPersons2 = repository.GetPersonsWithAge().Take(2); 
-var listPersons3 = repository.GetPersonsFullInfo().Take(2); 
+var listPersons1 = repository.GetPerson().Take(2); 
+var listPersons2 = repository.GetPersonWithAge().Take(2); 
+var listPersons3 = repository.GetPersonWithWithAgeAndName().Take(2); 
 
 Console.WriteLine();
 
-foreach (var person in listPersons1)
-{
-    Console.WriteLine($"{person.FirstName} {person.LastName} {person.Patronymic}"); 
-}
+PersonView.PrintPerson(listPersons1);
 
 Console.WriteLine();
 
-foreach (var person in listPersons2)
-{
-    Console.WriteLine($"{person.FirstName} {person.LastName} {person.Patronymic} : {person.Age}"); 
-}
+PersonView.PrintPersonWithAge(listPersons2);
 
 Console.WriteLine();
 
-foreach (var person in listPersons3)
-{
-    Console.WriteLine($"{person.FirstName} {person.LastName} {person.Patronymic} : {person.Age} - {person.PhoneNumber}"); 
-}
+PersonView.PrintPersonWithAgeAndName(listPersons3);
